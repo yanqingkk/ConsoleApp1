30f60f5711fbb107604b34a6fa26601dadbb939e
Yanqing <yanqing.zhu@education.nsw.gov.au>
2023-02-15 10:34:02 +1100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
+
+
+
+            //Console.WriteLine("Your input was: " + userINp);
+
+
+
+
+        }
+    }
+}
